d1addee1b1f34da0bbf867f00af993a3af2aea7b
Sergey Afonin <nindzya85@mail.ru>
2022-10-16 18:23:27 +0300

    DZ

---
+++ Desktop/DZ/Program.cs
@@ -0,0 +1,51 @@
+﻿/* 1. Задача
+
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2){
+    Console.WriteLine($"{num1} more than a number {num2}");
+}
+else{Console.WriteLine($"{num2} more than a number {num1}");
+}
+
+*/
+
+/* 2. Задача
+
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2>num1)
+max=num2;
+if (num3>max)
+max=num3;
+Console.WriteLine(max);
+
+*/
+
+/* 3. Задача
+
+Console.Write("Input first int number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+
+*/
+
+/* 4. Задача
+
+Console.Write("Input first int number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 0;
+while (current < num){
+    Console.Write(current += 2);
+    }
+
+*/    
